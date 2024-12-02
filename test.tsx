38de95e7fde3f8146065024cceeaa931dd587fce
Rahul Kumar Mishra <74014846+I-rahulmishra@users.noreply.github.com>
2024-12-02 13:40:40 +0530

    Update test.tsx

--- test.tsx
+++ test.tsx
@@ -3,3 +3,7 @@ return await dispatch(await preserveRequest(patchUserInputOnPayload(), currentSt
        }).catch((err: any) => {
         return Promise.reject(err);
       });
+
+
+appData.applicationdata.application.notification_required = true;
+          appData.applicationdata.application.is_save_to_pega = 'Yes';
